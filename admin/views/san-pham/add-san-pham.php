77b25787f35f949c0d9c7b1a25024a8b00481cfc
Đoàn Văn Long <47972513+doanvanlong@users.noreply.github.com>
2021-12-22 22:55:30 +0700

    c

--- admin/views/san-pham/add-san-pham.php
+++ admin/views/san-pham/add-san-pham.php
@@ -1,72 +1,67 @@
-
-    <div class=" formtitle" >
-        <h1>Thêm Mới Sản Phẩm</h1>
-    </div>
-    <div class="formcontent">
-        <form action="index.php?act=xu-ly-sp.php" method="post" enctype="multipart/form-data" name="add_san_pham">
-            <div class=" row mb10" style="margin-bottom: 10px;">
-                Danh mục:
-               <select name="ten_dm" id="" style="width:100px;margin-left: 52px;">
-               <?php
-               if (isset($_POST['ten_dm'])) {
-                      $ten_dm=$POST['id_ten_dm'];
-                      
-                        $sql="SELECT * FROM `danh_muc` WHERE 1";
-                         $info_dm=pdo_query_one($sql,$ten_dm);
-                        }
-                        ?>
-               <?php    
-              
-               foreach ($ten_dm as $key => $value){
-                echo $value;
-               }
-               ?>
-               </select>
-            </div>   
-        
-            <div class=" row mb10" style="margin-bottom: 10px;">
-                Mã sản phẩm:
-                <input type="text" name="id_sp"
-               style="margin-left: 32px;">
+<div class=" formtitle">
+    <h1>Thêm Mới Sản Phẩm</h1>
 </div>
+<div class="formcontent">
+    <form action="views/san-pham/xu-ly-sp.php" method="post" enctype="multipart/form-data" >
+        <div class=" row mb10" style="margin-bottom: 10px;">
+            Danh mục:
+            <select name="id_dm" id="" style="width:100px;margin-left: 52px;">
+                <?php
 
-            <div class=" row mb10" style="margin-bottom: 10px;">
-                Tên sản phẩm:
-                <input type="text" name="ten_sp"
- style="margin-left: 30px ;">
-            </div>
 
-            <div class=" row mb10" style="margin-bottom: 10px;">
-                Đơn giá:
-                <input type="text" name="gia_sp"
-style="margin-left: 67px;">
-            </div>
+                $sql = "SELECT * FROM `danh_muc` WHERE 1";
+                $list_dm = pdo_query($sql);
+                foreach ($list_dm as $dm){
+                    ?>
+                    <option name="id_dm" value="<?=$dm['id_dm']?>"><?=$dm['ten_dm']?></option>
+                    <?php
+                }
+                ?>
+                <?php
 
-            <div class=" row mb10" style="margin-bottom: 10px;">
-                Số Lượng:
-                <input type="text" name="so_luong"
-style="margin-left: 55px;">
-            </div>
 
-            <div class=" row mb10" style="margin-bottom: 10px;">
-                Hình:
-                <input type="file" name="hinh_anh"
-style="margin-left: 85px; border-radius:5px;">
-            </div>
-            <div class=" row mb10" >
-                Mô tả:
-                <textarea name="mo_ta" id="" cols="30" rows="10"style="margin-left: 80px; border-radius:5px;"></textarea>
-            </div>
+                ?>
+            </select>
+        </div>
 
-            <div class=" row mb10" style="margin-bottom: 10px;">
-                <input type="submit" name="themmoi" value="Thêm Mới">
-                <input type="reset" value="Nhập Lại">
-                <a href="#"><input type="button" value="Danh Sách"></a>
-            </div>
-           
-        </form>
- 
+        
+        <div class=" row mb10" style="margin-bottom: 10px;">
+            Tên sản phẩm:
+            <input type="text" name="ten_sp" style="margin-left: 30px ;">
+        </div>
 
-<script>
+        <div class=" row mb10" style="margin-bottom: 10px;">
+            Đơn giá:
+            <input type="text" name="gia_sp" style="margin-left: 67px;">
+        </div>
+
+        <div class=" row mb10" style="margin-bottom: 10px;">
+            Số Lượng:
+            <input type="text" name="so_luong" style="margin-left: 55px;">
+        </div>
+
+        <div class=" row mb10" style="margin-bottom: 10px;">
+            Hình:
+            <input type="file" name="hinh_anh" style="margin-left: 85px; border-radius:5px;">
+        </div>
+        <div class=" row mb10">
+            Mô tả:
+            <textarea name="mo_ta" id="" cols="30" rows="10" style="margin-left: 80px; border-radius:5px;"></textarea>
+        </div>
+
+        <div class=" row mb10" style="margin-bottom: 10px;">
+            <input type="submit" name="themmoi_sp" value="Thêm Mới">
+            <input type="reset" value="Nhập Lại">
+            <a href="#"><input type="button" value="Danh Sách"></a>
+        </div>
+
+    </form>
+
+
+
+
+
    
-</script>
+    <script>
+
+    </script>
--- admin/views/san-pham/xu-ly-sp.php
+++ admin/views/san-pham/xu-ly-sp.php
@@ -1,29 +1,22 @@
 <?php
 session_start();
-require_once '../../model/pdo.php';
-if (isset($_POST['add_san_pham'])) {
-    $id_sp = $_POST['id_sp'];
+require_once '../../../model/pdo.php';
+if (isset($_POST['themmoi_sp'])) {
+    $id_dm = $_POST['id_dm'];
     $ten_sp = $_POST['ten_sp'];
     $gia_sp = $_POST['gia_sp'];
     $so_luong = $_POST['so_luong'];
     $mo_ta = $_POST['mo_ta'];
-  
-    $sql = "INSERT INTO san_pham (ten_sp,gia_sp,so_luong,mo_ta) values(?,?,?,?)";
+    $hinh_anh=$_FILES['hinh_anh']['name'];
+    move_uploaded_file($_FILES['hinh_anh']['tmp_name'],'uploads/'.$hinh_anh);
+    $sql = "INSERT INTO san_pham (ten_sp,gia_sp,so_luong,mo_ta,hinh_anh,id_dm) values(?,?,?,?,?,?)";
     try {
-        pdo_execute($sql,$ten_sp ,$gia_sp, $so_luong,);
-        $_SESSION['thong-bao'] = "Thêm danh mục thành công";
+        pdo_execute($sql,$ten_sp ,$gia_sp, $so_luong,$mo_ta,$hinh_anh,$id_dm);
+        $_SESSION['thong-bao'] = "Thêm sản phẩm thành công";
         header('Location:../../index.php?action=san-pham');
     } catch (PDOException $e) {
-        $_SESSION['thong-bao'] = "Thêm danh mục thất bại";
+        $_SESSION['thong-bao'] = "Thêm sản phẩm thất bại";
         header('Location:../../index.php?action=san-pham');
     }
 }
 ?>
-<?php
-if (isset($_POST['ten_dm'])) {
-   $ten_dm=$POST['id_ten_dm'];
-   $sql="SELECT * FROM `danh_muc` WHERE 1";
-   $sql="UPDATE `danh_muc` SET `ten_dm`=? WHERE id_dm=?";
-   $info_dm=pdo_query_one($sql,$ten_dm);
-}
-\
